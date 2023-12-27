57e3b91528aa8710c985db24eec43d5386892e51
DESKTOP-7RB87TQ\spintechs <ryad.abderrahim@spintechs.net>
2023-12-27 14:36:32 +0100

    all script updated

--- sync_data_api.py
+++ sync_data_api.py
@@ -29,12 +29,12 @@ client_id = config.get('TalenteoAPI', 'client_id')
 client_secret = config.get('TalenteoAPI', 'client_secret')
 
 # Create a logger for business log
-buisness_logger = logging.getLogger('buisness_log')
-buisness_logger.setLevel(logging.INFO)
-buisness_handler = logging.FileHandler('buisness_log.log')
-buisness_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-buisness_handler.setFormatter(buisness_formatter)
-buisness_logger.addHandler(buisness_handler)
+business_logger = logging.getLogger('business_log')
+business_logger.setLevel(logging.INFO)
+business_handler = logging.FileHandler('business_log.log')
+business_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+business_handler.setFormatter(business_formatter)
+business_logger.addHandler(business_handler)
 
 # Create a logger for debug log
 debug_logger = logging.getLogger('debug_log')
@@ -49,27 +49,22 @@ def connect_to_database(config):
         connection = psycopg2.connect(**config)
         # business log
         logging.info("Connected to PostgreSQL database")
-        print("Connected to PostgreSQL database")
         return connection
     except Exception as e:
         # debug log
         logging.error(f"Error: Unable to connect to the database - {e}")
-        print(f"Error: Unable to connect to the database - {e}")
         return None
 
 def fetch_attendance_data(connection):
     try:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM attendance WHERE synced = false;")
-        attendance_data = cursor.fetchall()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM attendance WHERE synced = false;")
+            attendance_data = cursor.fetchall()
         return attendance_data
     except Exception as e:
         # debug log
         logging.error(f"Error: Unable to fetch attendance data - {e}")
-        print(f"Error: Unable to fetch attendance data - {e}")
         return None
-    finally:
-        cursor.close()
 
 def send_data_to_api(api_url, access_token, employee_id, datetime, timezone, note):
     headers = {
@@ -77,9 +72,7 @@ def send_data_to_api(api_url, access_token, employee_id, datetime, timezone, not
         "Content-Type": "application/json",
     }
 
-    # Convert datetime object to a formatted string
     formatted_datetime = datetime.strftime("%Y-%m-%d %H:%M")
-
     data = {
         "datetime": formatted_datetime,
         "timezone": timezone,
@@ -89,51 +82,29 @@ def send_data_to_api(api_url, access_token, employee_id, datetime, timezone, not
     try:
         response = requests.post(f"{api_url}/api/v1/employee/{employee_id}/punch", headers=headers, json=data)
         response.raise_for_status()
-        # Log information about the synchronized data and payload
-        print(response)
         # business log
-        buisness_logger.info(f"Synchronized data for employee ID {employee_id}")
+        business_logger.info(f"Synchronized data for employee ID {employee_id}")
         # debug log
         debug_logger.debug(f"Synchronized data for employee ID {employee_id} with payload: {data}")
         return response.json()
-    except requests.exceptions.HTTPError as errh:
-        # debug log
-        debug_logger.debug(f"HTTP Error when sending data to the API : {errh}")
-        print(f"HTTP Error when sending data to the API : {errh}")
-        logging.error(f"HTTP Error when sending data to the API : {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        debug_logger.debug(f"Error connecting when sending data to the API : {errc}")
-        print(f"Error Connecting when sending data to the API : {errc}")
-        logging.error(f"Error Connecting when sending data to the API : {errc}")
-    except requests.exceptions.Timeout as errt:
-        debug_logger.debug(f"Error connecting when sending data to the API : {errt}")
-        print(f"Timeout Error when sending data to the API : {errt}")
-        logging.error(f"Timeout Error when sending data to the API : {errt}")
     except requests.exceptions.RequestException as err:
-        # log error if synchronization fails
-        debug_logger.debug(f"Error : Unable to send data to API - {err}")
-        print(f"Request Exception when sending data to the API : {err}")
-        buisness_logger.error(f"Error : unable to send data to API - {err}")
-        logging.error(f"Error : unable to send data to API - {err}")  
-    return None
+        log_message = f"Error: Unable to send data to API for employee ID {employee_id} - {err}"
+        debug_logger.debug(log_message)
+        business_logger.error(log_message)
+        return None
 
 def update_sync_status(connection, attendance_id):
     try:
-        cursor = connection.cursor()
-        cursor.execute("UPDATE attendance SET synced = true WHERE attendance_id = %s;", (attendance_id,))
-        connection.commit()
-        logging.info(f"Sync status updates for attendance ID : {attendance_id}")
-        print(f"Sync status updated for attendance ID: {attendance_id}")
+        with connection.cursor() as cursor:
+            cursor.execute("UPDATE attendance SET synced = true WHERE attendance_id = %s;", (attendance_id,))
+            connection.commit()
+        logging.info(f"Sync status updated for attendance ID: {attendance_id}")
     except Exception as e:
-        logging.error(f"Error : unable to update sync status - {e}")
-        print(f"Error: Unable to update sync status - {e}")
-    finally:
-        cursor.close()
+        logging.error(f"Error: Unable to update sync status - {e}")
 
-# checking if the data is available on the api
 def check_sync_status(api_url, access_token, attendance_id):
     headers = {
-        "Authorization": f"Bearer{access_token}",
+        "Authorization": f"Bearer {access_token}",
         "Content_Type": "application/json"
     }
 
@@ -144,35 +115,15 @@ def check_sync_status(api_url, access_token, attendance_id):
         data = response.json()
         if data.get("success") and data.get("data", {}).get("synced"):
             logging.info(f"Attendance ID {attendance_id} already synchronized on the API. Skipping")
-            print(f"Attendance ID {attendance_id} already synchronized on the API. Skipping")
             return True
         else:
             logging.info(f"Attendance ID {attendance_id} not synchronized on the API. Retrying sync.")
-            print(f"Attendance ID {attendance_id} not synchronized on the API. Retrying sync.")
             return False
-    except requests.exceptions.HTTPError as errh:
-        if errh.response.status_code == 404:
-            logging.info(f"Attendance ID {attendance_id} not found on the API. Retrying sync.")
-            print(f"Attendance ID {attendance_id} not found on the API. Retrying sync.")
-            return False
-        else:
-            logging.error(f"HTTP Error when checking if the data is synchronized: {errh}")
-            print(f"HTTP Error when checking if the data is synchronized: {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        logging.error(f"Error Connecting when checking if the data is synchronized: {errc}")
-        print(f"Error Connecting when checking if the data is synchronized: {errc}")
-    except requests.exceptions.Timeout as errt:
-        logging.error(f"Timeout Error when checking if the data is synchronized: {errt}")
-        print(f"Timeout Error when checking if the data is synchronized: {errt}")
     except requests.exceptions.RequestException as err:
-        logging.error(f"Request Exception Error: {err}")
-        print(f"Request Exception Error: {err}")
-    # If reach this point, there was an issue with the api request or the status check
-    logging.error(f"Unable to check synchronization status for attendance ID {attendance_id}")
-    print(f"Unable to check synchronization status for attendance ID {attendance_id}")
-    return False
+        log_message = f"Error when checking if the data is synchronized for Attendance ID {attendance_id}: {err}"
+        logging.error(log_message)
+        return False
 
-# checking if the data is sync after the sync process
 def check_sync_status_after_sync(api_url, access_token, attendance_id):
     headers = {
         "Authorization": f"Bearer {access_token}",
@@ -187,101 +138,66 @@ def check_sync_status_after_sync(api_url, access_token, attendance_id):
 
         if data.get("success") and data.get("data", {}).get("synced"):
             logging.info(f"Attendance ID {attendance_id} synchronized successfully on the API.")
-            print(f"Attendance ID {attendance_id} synchronized successfully on the API.")
             return True
         else:
             logging.error(f"Failed to synchronize Attendance ID {attendance_id} on the API.")
-            print(f"Failed to synchronize Attendance ID {attendance_id} on the API.")
             return False
-    except requests.exceptions.HTTPError as errh:
-        if errh.response.status_code == 404:
-            logging.info(f"Attendance ID {attendance_id} not found on the API. Retrying sync.")
-            print(f"Attendance ID {attendance_id} not found on the API. Retrying sync.")
-            return False
-        else:
-            logging.error(f"HTTP Error when checking if the data is synchronized: {errh}")
-            print(f"HTTP Error when checking if the data is synchronized: {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        logging.error(f"Error Connecting when checking if the data is synchronized: {errc}")
-        print(f"Error Connecting when checking if the data is synchronized: {errc}")
-    except requests.exceptions.Timeout as errt:
-        logging.error(f"Timeout Error when checking if the data is synchronized: {errt}")
-        print(f"Timeout Error when checking if the data is synchronized: {errt}")
     except requests.exceptions.RequestException as err:
-        logging.error(f"Request Exception Error: {err}")
-        print(f"Request Exception Error: {err}")
-    # If reach this point, there was an issue with the api request or the status check
-    print(f"Unable to check synchronization status for attendance ID {attendance_id}")
-    return False
+        log_message = f"Error when checking if the data is synchronized for Attendance ID {attendance_id}: {err}"
+        logging.error(log_message)
+        return False
 
 def sync_data_in_date_range(start_date, end_date, connection, api_url, access_token):
     date_increment = datetime.timedelta
     current_date = start_date
+
     while current_date <= end_date:
         try:
-            cursor = connection.cursor()
-            cursor.execute("SELECT * FROM attendance WHERE date(datetime) = %s AND synced = false;", (current_date,))
-            attendance_data = cursor.fetchall()
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM attendance WHERE date(datetime) = %s AND synced = false;", (current_date,))
+                attendance_data = cursor.fetchall()
 
-            for row in attendance_data:
-                attendance_id, employee_id, datetime_value, timezone, note, synced = row
+                for row in attendance_data:
+                    attendance_id, employee_id, datetime_value, timezone, note, synced = row
 
-                # Map the local database ID to the corresponding Talenteo employee ID
-                talenteo_employee_id = id_mapping.get(employee_id)
+                    talenteo_employee_id = id_mapping.get(employee_id)
 
-                # Skip if there's no mapping for the employee
-                if talenteo_employee_id is None:
-                    logging.info(f"No mapping found for local DB ID: {employee_id}. Skipping.")
-                    print(f"No mapping found for local DB ID: {employee_id}. Skipping.")
-                    continue
+                    if talenteo_employee_id is None:
+                        logging.info(f"No mapping found for local DB ID: {employee_id}. Skipping.")
+                        continue
 
-                # Send data to the Talenteo API
-                api_response = send_data_to_api(api_url, access_token, talenteo_employee_id, datetime_value, timezone, note)
+                    api_response = send_data_to_api(api_url, access_token, talenteo_employee_id, datetime_value, timezone, note)
 
-                # If API response is successful, check if the data is synchronized after the sync process
-                if api_response and api_response.get("success"):
-                    update_sync_status(connection, attendance_id)
-                    # Check if the data is synchronized after the sync process
-                    check_sync_status_after_sync(api_url, access_token, attendance_id)
-                else:
-                    # If syncing fails, log an error and retry after a delay
-                    logging.error(
-                        f"Failed to sync data for attendance ID {attendance_id}, Retrying in 5 seconds...")
-                    print(
-                        f"Failed to sync data for attendance ID {attendance_id}, Retrying in 5 seconds...")
-                    time.sleep(5)
+                    if api_response and api_response.get("success"):
+                        update_sync_status(connection, attendance_id)
+                        check_sync_status_after_sync(api_url, access_token, attendance_id)
+                    else:
+                        logging.error(f"Failed to sync data for attendance ID {attendance_id}. Retrying in 5 seconds...")
+                        time.sleep(5)
 
-            logging.info(f"All attendance data for the date {current_date} synchronized successfully.")
-            print(f"All attendance data for the date {current_date} synchronized successfully.")
+                logging.info(f"All attendance data for the date {current_date} synchronized successfully.")
 
         except Exception as e:
-            logging.error(f"Error: Unable to sync data for date - {e}")
-            print(f"Error: Unable to sync data for date - {e}")
+            logging.error(f"Error: Unable to sync data for date {current_date} - {e}")
         finally:
-            cursor.close()
             current_date += date_increment(days=1)
 
 def main():
-    # Get access token
     access_token = get_access_token(api_url, client_id, client_secret)
     if not access_token:
         logging.error("Error: Unable to obtain access token.")
         print("Error: Unable to obtain access token.")
         return
 
-    # Connect to the local database
     postgres_connection = connect_to_database(db_config)
     if not postgres_connection:
         return
 
-    # Set the date range for synchronization
-    start_date = datetime.date(2023, 12, 25)
-    end_date = datetime.date(2023, 12, 25)
+    start_date = datetime.date(2023, 12, 27)
+    end_date = datetime.date(2023, 12, 27)
 
-    # Sync data for the specified date range
     sync_data_in_date_range(start_date, end_date, postgres_connection, api_url, access_token)
 
-    # Close the database connection
     postgres_connection.close()
 
 if __name__ == "__main__":
--- zk_connect.py
+++ zk_connect.py
@@ -1,129 +1,126 @@
 import psycopg2
-from zk import ZK
 import configparser
+import logging
+from datetime import datetime
+from zk import ZK
 
-# The Configuration
+# Set up logging configuration at the beginning of the script
+logging.basicConfig(level=logging.DEBUG)
+
+# Function to configure and get a logger
+def setup_logger(name, level, file_name):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.FileHandler(file_name)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+# Create loggers
+business_logger = setup_logger('business_log_zk', logging.INFO, 'business_log.log')
+debug_logger = setup_logger('debug_log_zk', logging.DEBUG, 'debug_log.log')
+
+# Function to read the configuration
 def read_config(file_path='config.ini'):
     config = configparser.ConfigParser()
     config.read(file_path)
     return config
 
+# Function to get device configuration
 def get_device_config():
     config = read_config()
-    
-    # Debug print to see available sections
-    print("Sections in config:", config.sections())
-
-    # Case-insensitive search for 'Device' section
+    logging.debug('Sections in config: %s', config.sections())
     for section in config.sections():
         if section.lower() == 'device1':
             return config[section]
-
-    # If 'Device' section is not found
     raise KeyError('Device section not found in the configuration file')
 
+# Function to get PostgreSQL configuration
 def get_postgres_config():
     config = read_config()
     return config['Postgres']
 
+# Function to connect to PostgreSQL
 def connect_to_postgresql(postgres_config):
     try:
-        connection = psycopg2.connect(
-            database=postgres_config['database'],
-            user=postgres_config['user'],
-            password=postgres_config['password'],
-            host=postgres_config['host'],
-            port=postgres_config['port']
-        )
-        print("Connected to PostgreSQL database")
+        connection = psycopg2.connect(**postgres_config)
+        logging.info("Connected to Database")
         return connection
     except Exception as e:
-        print(f"Error: Unable to connect to the database - {e}")
+        logging.error("Error: Unable to connect to the database - %s", e)
         return None
 
+# Function to register attendance to the database
 def register_attendance_to_database(connection, talenteo_id, datetime, timezone, note, is_sync):
     try:
-        cursor = connection.cursor()
-        # check if the attendance log alredy exists
-        cursor.execute("""
-            SELECT * FROM attendance
-            WHERE employee_id = %s AND datetime = %s;
-        """, (talenteo_id, datetime))
-        existing_log = cursor.fetchone()
-
-        if existing_log:
-            print("Attendance log already exists. Skipping registration.")
-        else:
+        with connection.cursor() as cursor:
             cursor.execute("""
-                INSERT INTO attendance (
-                    employee_id, 
-                    datetime, 
-                    timezone, 
-                    note, 
-                    synced
-                ) 
-                VALUES (%s, %s, %s, %s, %s)
-            """, (
-                talenteo_id, datetime, timezone, note, is_sync
-            ))
-            connection.commit()
-            print("Attendance log registered successfully")
+                SELECT * FROM attendance
+                WHERE employee_id = %s AND datetime = %s;
+            """, (talenteo_id, datetime))
+            existing_log = cursor.fetchone()
+
+            if existing_log:
+                business_logger.info("Attendance log already exists. Skipping registration.")
+            else:
+                cursor.execute("""
+                    INSERT INTO attendance (
+                        employee_id, 
+                        datetime, 
+                        timezone, 
+                        note, 
+                        synced
+                    ) 
+                    VALUES (%s, %s, %s, %s, %s)
+                """, (talenteo_id, datetime, timezone, note, is_sync))
+                connection.commit()
+                business_logger.info("Attendance log registered successfully")
     except Exception as e:
-        print(f"Error: Unable to register attendance - {e}")
-    finally:
-        cursor.close()
+        logging.error("Error: Unable to register attendance - %s", e)
 
+# Main function
 def main():
-    # Get PostgreSQL Database Configuration
     postgres_config = get_postgres_config()
-
-    # Connect to PostgreSQL
     postgres_connection = connect_to_postgresql(postgres_config)
 
     if not postgres_connection:
         return
 
-    # Get device configuration from the configuration file
     try:
         device_config = get_device_config()
     except KeyError as e:
-        print(f"Error: {e}")
+        logging.error("Error: %s", e)
         return
 
     device_ip = device_config.get('device_ip', 'localhost')
     device_port = int(device_config.get('device_port', 4370))
 
     try:
-        # Connect to ZKTeco Device
         zk = ZK(device_ip, port=device_port, timeout=5)
         zk.connect()
         zk.enable_device()
 
-        # Get attendance logs from ZKTeco Device
         attendance_logs = zk.get_attendance()
 
-        # Register attendance logs to PostgreSQL
         for log in attendance_logs:
             talenteo_id = log.user_id
             timezone = "Europe/London" 
             note = "Punch note" 
-            datetime = log.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            log_datetime = log.timestamp.strftime("%Y-%m-%d %H:%M:%S")
             is_sync = False
 
             register_attendance_to_database(
                 postgres_connection,
                 talenteo_id,
-                datetime,
+                log_datetime,
                 timezone,
                 note,
                 is_sync
             )
 
     finally:
-        # Disconnect from ZKTeco Device
         zk.disconnect()
-
-        # Close PostgreSQL connection
         postgres_connection.close()
 
 if __name__ == "__main__":
